6f3b74815c88456d7685e23eb74d29c121620be1
Rafhael Cristian <rafael_cristian_@hotmail.com>
2023-08-02 17:16:00 -0300

    Fix: cors

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,11 @@ import { handleErros } from "./error";
 import clientRoutes from "./routes/client.routes";
 import contactRoutes from "./routes/contact.routes";
 import loginRoutes from "./routes/session.routes";
+import cors from "cors";
 
 const app: Application = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/client", clientRoutes);
 app.use("/login", loginRoutes);
 app.use("/contact", contactRoutes);
